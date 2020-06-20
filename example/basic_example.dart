265d331feacbff521c232e34ac98702e699e8dad
Gavan Singh <gav@gavans.work>
2020-06-20 14:49:46 +1000

    fix typo in example

--- example/basic_example.dart
+++ example/basic_example.dart
@@ -2,12 +2,12 @@ import 'package:faunadb_http/faunadb_http.dart';
 
 /*
 * FaunaDB secret should be the first argument
-* `dart ./basic_example.dart <you_secret_here>`
+* `dart ./basic_example.dart <your_secret_here>`
 * */
 void main(List<String> arguments) async {
   final config = FaunaDBConfig.build(secret: arguments[0]);
   // Or if you want to put your secret straight in your code
-  // final config = FaunaDBConfig.build(secret: "<you_secret_here>");
+  // final config = FaunaDBConfig.build(secret: "<your_secret_here>");
   final client = FaunaDBClient(config);
 
   var value = await client.query({
