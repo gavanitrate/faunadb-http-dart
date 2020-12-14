fa6f4de442aa327e825a4b2c2dde54af1666e244
Gavan Singh <gav@gavans.work>
2020-12-14 15:51:36 +1100

    update example

--- example/main.dart
+++ example/main.dart
@@ -13,7 +13,7 @@ void main(List<String> arguments) async {
   // final config = FaunaDBConfig.build(secret: '<your_secret_here>');
   final client = FaunaClient(config);
 
-  final query = Collections();
+  final query = Paginate(Collections());
   print('query=>\n${query}');
 
   final response = await client.query(query);
